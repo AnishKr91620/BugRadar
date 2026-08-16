682f745074afb7e3074512d2c6d33e3ef52831dc
AnishKr91620 <anish91620@gmail.com>
2026-08-16 17:16:46 +0530

    chore: update Aiven MySQL connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,15 +2,21 @@ const mysql = require("mysql2/promise");
 const fs = require("fs");
 require("dotenv").config();
 
+const path = require("path");
+
+const caCertPath = path.resolve(__dirname, "../../certs/ca.pem");
+
 const poll = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    ca: fs.readFileSync("./certs/ca.pem", "utf8"),
-  },
+  ssl: fs.existsSync(caCertPath)
+    ? {
+        ca: fs.readFileSync(caCertPath, "utf8"),
+      }
+    : undefined,
   waitForConnections: true,
   connectionLimit: 10,
 });
